3850ced25aed89efa858d89144b2fd3a499a75c5
tleach01 <taylorleach0@gmail.com>
2020-08-31 09:44:16 -0400

    still having post errors

--- script.js
+++ script.js
@@ -1,9 +1,88 @@
-const noteDiv = document.querySelector('#notes')
+
+let numberOfNotes;
+
+const noteText = document.querySelector('#formNoteText')
 const addButton = document.querySelector('#formAddButton')
-const delButton = document.querySelector('#formDelButton')
-const newNote = document.querySelector('#formNoteText')
+const color = document.querySelector('#formColor')
+const notes = document.querySelector('#notes')
+noteText.focus()
 
+function addNote () {
+  const text = noteText.value
+  const note = document.createElement('div')
+  const deleteButton = document.createElement('button')
 
-fetch('http://localhost:3000/notes/')
-.then(res => res.json())
-.then(mood)
+  note.classList.add('note')
+  note.classList.add(color.value)
+  note.innerHTML = `<i class="fas fa-thumbtack"></i> <div class='note-text'>${text} </div>`
+  deleteButton.innerHTML = 'Delete'
+  deleteButton.classList.add('delButton')
+
+  // deleteButton.addEventListener('click', function () {
+  //   fetch('http://localhost:3000/notes/',{
+  //     method: 'DELETE'
+  //   })
+  // })
+
+  note.appendChild(deleteButton)
+  notes.appendChild(note)
+
+  noteText.value = ''
+  noteText.focus()
+}
+
+addButton.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (noteText.value !== '') {
+    const body = {
+      id: numberOfNotes,
+      title: '',
+      body: noteText.value
+    }
+    addNote()
+    console.log(body)
+    postNote(body)
+  }
+})
+
+async function loadNotes () {
+  const response = await fetch('http://localhost:3000/notes/', {
+    method: 'GET'
+  })
+    .then(res => {
+      return res.json() })
+  numberOfNotes = response.length
+  for (let i = 0; i < response.length; i++) {
+    displayNote(response[i])
+  }
+}
+
+async function postNote(note) {
+  await fetch('http://localhost:3000/notes/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json'},
+    body: note
+  })
+    .then(res => console.log(res))
+}
+
+window.addEventListener('load', loadNotes())
+
+function displayNote (pageNote) {
+  console.log(pageNote)
+  const text = pageNote.body
+  const note = document.createElement('div')
+  const deleteButton = document.createElement('button')
+
+  note.classList.add('note')
+  note.classList.add(color.value)
+  note.innerHTML = `<i class="fas fa-thumbtack"></i> <div class='note-text'>${text} </div>`
+  deleteButton.innerHTML = 'Delete'
+  deleteButton.classList.add('delButton')
+
+  note.appendChild(deleteButton)
+  notes.appendChild(note)
+
+  noteText.value = ''
+  noteText.focus()
+}
